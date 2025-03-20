e987ee15ca0dccb1b38a10316a9dcdd711e3b23d
Tyra <tyra.mwai@student.moringaschool.com>
2025-03-20 23:57:28 +0300

    Added Changes

--- READ.md
+++ READ.md
@@ -1,1 +1,5 @@
 https://3tyra.github.io/phase-1-Ramen-Rating-code-challenge/
+
+
+
+ 
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg", rating: 5, comment: "Rich and creamy broth!" },
-    { id: 4, name: "Beef Ramen", restaurant: "Shibuya", image: "Beef.jpg", rating: 7, comment: "Amazing!"},
-    { id: 5, name: "pork Ramen", restaurant: "Shiganshi", image: "pork.jpg", rating: 3, comment: "The pork was tough and a bit chewy!"},
-
-]
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran Restaurant", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya Restaurant", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya Restaurant", image: "tonkotsu.jpg", rating: 5, comment: "Rich and creamy broth!" },
+    { id: 4, name: "Beef Ramen", restaurant: "Shibuya Restaurant", image: "Beef.jpg", rating: 7, comment: "Amazing!" },
+    { id: 5, name: "Pork Ramen", restaurant: "Shigansh Restaurant", image: "pork.jpg", rating: 3, comment: "The pork was tough and a bit chewy!" }
+];
 
 function displayRamens() {
     const ramenMenu = document.getElementById("ramen-menu");
@@ -19,12 +18,11 @@ function displayRamens() {
         ramenMenu.appendChild(img);
     });
 
-    
+   
     if (ramens.length > 0) {
         handleClick(ramens[0]);
     }
 }
-
 
 function handleClick(ramen) {
     document.getElementById("ramen-image").src = ramen.image;
@@ -34,24 +32,38 @@ function handleClick(ramen) {
     document.getElementById("ramen-comment").textContent = ramen.comment;
 }
 
-
 function addSubmitListener() {
-    const form = document.getElementById("new-ramen");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
+    document.querySelector("#new-ramen button").addEventListener("click", function(event) {
+        event.preventDefault();  
 
         const newRamen = {
             id: ramens.length + 1,
             name: document.getElementById("new-name").value,
-            restaurant: document.getElementById("new-restaurant").value,
-            image: document.getElementById("new-image").value,
-            rating: document.getElementById("new-rating").value,
-            comment: document.getElementById("new-comment").value
+            restaurant: document.getElementById("new-restaurant").value, 
+            image: document.getElementById("new-image").value, 
+            rating: document.getElementById("new-rating").value, 
+            comment: document.getElementById("new-comment").value 
         };
 
+
         ramens.push(newRamen);
-        displayRamens();
-        form.reset();
+
+       
+        const img = document.createElement("img");
+        img.src = newRamen.image;
+        img.alt = newRamen.name;
+        img.addEventListener("click", () => handleClick(newRamen));
+        document.getElementById("ramen-menu").appendChild(img);
+
+        
+        handleClick(newRamen);
+
+       
+        document.getElementById("new-name").value = "";
+        document.getElementById("new-restaurant").value = "";
+        document.getElementById("new-image").value = "";
+        document.getElementById("new-rating").value = "";
+        document.getElementById("new-comment").value = "";
     });
 }
 
@@ -60,6 +72,4 @@ function main() {
     addSubmitListener();
 }
 
-
 document.addEventListener("DOMContentLoaded", main);
-
